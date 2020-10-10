6188c73cda48fe5653c0cf37de33ce0a9f94d92a
Sean Sundberg <seansund@us.ibm.com>
2020-10-10 08:23:46 -0500

    Fixes webhook-patch-ca-bundle.sh to handle multiple tokens with CA certs

--- bin/webhook-patch-ca-bundle.sh
+++ bin/webhook-patch-ca-bundle.sh
@@ -9,7 +9,23 @@ set -o pipefail
 CA_BUNDLE=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
 
 if [ -z "${CA_BUNDLE}" ]; then
-    CA_BUNDLE=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].data.ca\.crt}")
+    SECRET_NAMES=$(kubectl get secrets -o jsonpath="{.items[?(@.metadata.annotations['kubernetes\.io/service-account\.name']=='default')].metadata.name}" | tr " " "\n")
+
+    TMP_DIR="${PWD}/tmp-ca-bundle"
+    mkdir -p "${TMP_DIR}"
+
+    CA_BUNDLE_FILE="${TMP_DIR}/ca-bundle"
+
+    echo "${SECRET_NAMES}" | while read secret; do
+      ca_bundle=$(kubectl get secrets "${secret}" -o jsonpath="{.data.ca\.crt}" | base64 -D)
+      if [[ -n "${ca_bundle}" ]]; then
+        echo "${ca_bundle}" >> "${CA_BUNDLE_FILE}"
+      fi
+    done
+
+    CA_BUNDLE=$(cat "${CA_BUNDLE_FILE}" | base64)
+
+    rm -rf "${TMP_DIR}"
 fi
 
 export CA_BUNDLE NAMESPACE
